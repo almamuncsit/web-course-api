cd25cc4a58105eceba2643d937fecea33549f87a
Al-Mamun Sarkar <almamun.csit@gmail.com>
2018-09-16 01:07:12 +0600

    Configured radis and queue for email sending

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,14 @@
 
 Route::group(['middleware'=> 'web', 'prefix' => 'course', 'namespace' => 'WebCourse\Http\Controllers'], function() {
 
-    Route::post('/registration', 'Auth\RegistrationController@create')->name('course.register');
+//    Route::post('/registration', 'Auth\RegistrationController@create')->name('course.register');
+
+    Route::get('set', function (){
+        Redis::set('name', 'Mamun sarkar asdl');
+    });
+
+    Route::get('get', function (){
+        return Redis::get('name');
+    });
 
 });
